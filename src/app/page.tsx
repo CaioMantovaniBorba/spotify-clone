9a8e2dd7bed4d616282ff8278d585024e24d60df
CaioMantovaniBorba <caiovmborba@gmail.com>
2023-05-19 08:37:11 -0300

    Add player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react';
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Laptop2, LayoutList, Mic2, Volume, Maximize2 } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -36,7 +36,7 @@ export default function Home() {
             </button>
           </div>
 
-          <h2 className="font-semibold text-3xl nt-10">Good Afternoon</h2>
+          <h2 className="font-semibold text-3xl mt-10">Good Afternoon</h2>
 
           <div className="grid grid-cols-3 gap-4 mt-4">
             <a className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
@@ -83,7 +83,7 @@ export default function Home() {
             </a>
           </div>
 
-          <h1 className="font-semibold text-2xl nt-10">Made for Caio Mantovani Borba</h1>
+          <h1 className="font-semibold text-2xl mt-10">Made for Caio Mantovani Borba</h1>
 
           <div className="grid grid-cols-8 gap-4 mt-4">
             <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
@@ -115,7 +115,47 @@ export default function Home() {
         </main>
       </div>
 
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">FOOTER</footer>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <img src="/album.jpg" width={56} height={56} alt="Capa do album" />
+          <div className='flex flex-col'>
+            <strong className="font-normal">Rope</strong>
+            <span className="text-xs text-zinc-400">Foo Fighters</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black"><Play /></button>
+
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">2:14</span>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2 size={20} />
+          <div className="flex items-center gap-2">
+            <Volume size={20} />
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize2 size={20} />
+        </div>
+      </footer>
     </div>
   )
 }
